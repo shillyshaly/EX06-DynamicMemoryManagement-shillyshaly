206f8c2b278f60a0fd5c41255854ff7bd966ef09
vscilab <elvisseveriano2252@gmail.com>
2019-12-02 14:29:10 -0800

    EXCELSIOR!!!

--- src/csv30.cc
+++ src/csv30.cc
@@ -10,8 +10,12 @@ size_t LeakProofClass::_arrays = 0;
 
 void doDynamicMemoryManagement() {
     // Put your code here to allocate a single LeakProofClass instance
+    LeakProofClass *obj1 = new LeakProofClass();
 
     // Put your code here to allocate an array of LeakProofClass instances
+    LeakProofClass *arr = new LeakProofClass[5];
 
     // Be sure to delete all instances created in this function here.
+    delete(obj1);
+    delete[](arr);
 }
